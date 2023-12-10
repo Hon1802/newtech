2de50dc49ff41d0b6ea4553ebb10b540efe1c99f
Nguyễn Văn Hơn <74673759+Hon1802@users.noreply.github.com>
2023-12-10 13:42:37 +0700

    .
    .

--- src/server.js
+++ src/server.js
@@ -34,13 +34,19 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 //cors
-const allowedOrigins = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'http://localhost:6969',
-    'http://127.0.0.1:6969',
-  ];
-  app.use(cors());
+
+const allowedOrigins = {
+    origin: [
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
+      'http://localhost:6969',
+      'http://127.0.0.1:6969',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true, 
+  };
+app.use(cors(allowedOrigins));
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
